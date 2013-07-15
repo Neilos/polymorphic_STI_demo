cbf97201658eb0db3d8d0cf7cc53642f3b42e70d
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-07-15 10:52:17 +0100

    base models and controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ PolymorphicStiDemo::Application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :people
+  resources :courses
+  resources :faculties
 
   # Example resource route with options:
   #   resources :products do
