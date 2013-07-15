6435d6536a7d95d35eb46a75c3d562d9c42f3a18
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-07-15 10:31:12 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PolymorphicStiDemo::Application.config.secret_key_base = '089a7471b0d0c50082e148abeb58925ee3c170c9d617e6425130097deb84d4768daf86ea32b9953318832ed1599637f642f04ddd3acaa9a877f8f825d949267c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PolymorphicStiDemo::Application.config.session_store :cookie_store, key: '_polymorphic_STI_demo_session'
